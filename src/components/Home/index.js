ac9787c9881f4a9b063503b0bbe61ce7e508f96a
William A Rodriguez <wrod@mit.edu>
2020-04-19 17:24:05 -0400

    Demo @nivo/line chart

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 
-import { FirebaseCondiv } from '../Firebase';
-
+import { ResponsiveLine } from '@nivo/line'
 
 const BLOCK_WIDTH = 100
 const BLOCK_HEIGHT = 60
@@ -53,8 +52,350 @@ class Home extends Component{
             timestamp:0,
             saludTimeSignature:0}
 
-    console.log("PRops are")
-    console.log(props)
+  }
+
+  getLineChart = () =>{
+
+    const data = [
+  {
+    "id": "japan",
+    "color": "hsl(82, 70%, 50%)",
+    "data": [
+      {
+        "x": "plane",
+        "y": 250
+      },
+      {
+        "x": "helicopter",
+        "y": 255
+      },
+      {
+        "x": "boat",
+        "y": 48
+      },
+      {
+        "x": "train",
+        "y": 212
+      },
+      {
+        "x": "subway",
+        "y": 39
+      },
+      {
+        "x": "bus",
+        "y": 251
+      },
+      {
+        "x": "car",
+        "y": 72
+      },
+      {
+        "x": "moto",
+        "y": 64
+      },
+      {
+        "x": "bicycle",
+        "y": 226
+      },
+      {
+        "x": "horse",
+        "y": 211
+      },
+      {
+        "x": "skateboard",
+        "y": 119
+      },
+      {
+        "x": "others",
+        "y": 276
+      }
+    ]
+  },
+  {
+    "id": "france",
+    "color": "hsl(269, 70%, 50%)",
+    "data": [
+      {
+        "x": "plane",
+        "y": 132
+      },
+      {
+        "x": "helicopter",
+        "y": 189
+      },
+      {
+        "x": "boat",
+        "y": 224
+      },
+      {
+        "x": "train",
+        "y": 126
+      },
+      {
+        "x": "subway",
+        "y": 186
+      },
+      {
+        "x": "bus",
+        "y": 176
+      },
+      {
+        "x": "car",
+        "y": 115
+      },
+      {
+        "x": "moto",
+        "y": 136
+      },
+      {
+        "x": "bicycle",
+        "y": 94
+      },
+      {
+        "x": "horse",
+        "y": 285
+      },
+      {
+        "x": "skateboard",
+        "y": 125
+      },
+      {
+        "x": "others",
+        "y": 9
+      }
+    ]
+  },
+  {
+    "id": "us",
+    "color": "hsl(228, 70%, 50%)",
+    "data": [
+      {
+        "x": "plane",
+        "y": 282
+      },
+      {
+        "x": "helicopter",
+        "y": 59
+      },
+      {
+        "x": "boat",
+        "y": 74
+      },
+      {
+        "x": "train",
+        "y": 189
+      },
+      {
+        "x": "subway",
+        "y": 171
+      },
+      {
+        "x": "bus",
+        "y": 178
+      },
+      {
+        "x": "car",
+        "y": 277
+      },
+      {
+        "x": "moto",
+        "y": 292
+      },
+      {
+        "x": "bicycle",
+        "y": 15
+      },
+      {
+        "x": "horse",
+        "y": 81
+      },
+      {
+        "x": "skateboard",
+        "y": 16
+      },
+      {
+        "x": "others",
+        "y": 1
+      }
+    ]
+  },
+  {
+    "id": "germany",
+    "color": "hsl(339, 70%, 50%)",
+    "data": [
+      {
+        "x": "plane",
+        "y": 240
+      },
+      {
+        "x": "helicopter",
+        "y": 56
+      },
+      {
+        "x": "boat",
+        "y": 128
+      },
+      {
+        "x": "train",
+        "y": 2
+      },
+      {
+        "x": "subway",
+        "y": 100
+      },
+      {
+        "x": "bus",
+        "y": 56
+      },
+      {
+        "x": "car",
+        "y": 294
+      },
+      {
+        "x": "moto",
+        "y": 89
+      },
+      {
+        "x": "bicycle",
+        "y": 273
+      },
+      {
+        "x": "horse",
+        "y": 140
+      },
+      {
+        "x": "skateboard",
+        "y": 89
+      },
+      {
+        "x": "others",
+        "y": 249
+      }
+    ]
+  },
+  {
+    "id": "norway",
+    "color": "hsl(256, 70%, 50%)",
+    "data": [
+      {
+        "x": "plane",
+        "y": 239
+      },
+      {
+        "x": "helicopter",
+        "y": 44
+      },
+      {
+        "x": "boat",
+        "y": 118
+      },
+      {
+        "x": "train",
+        "y": 72
+      },
+      {
+        "x": "subway",
+        "y": 217
+      },
+      {
+        "x": "bus",
+        "y": 61
+      },
+      {
+        "x": "car",
+        "y": 215
+      },
+      {
+        "x": "moto",
+        "y": 130
+      },
+      {
+        "x": "bicycle",
+        "y": 41
+      },
+      {
+        "x": "horse",
+        "y": 72
+      },
+      {
+        "x": "skateboard",
+        "y": 289
+      },
+      {
+        "x": "others",
+        "y": 65
+      }
+    ]
+  }
+]
+    console.log(`Line data is ${data}`)
+    const MyResponsiveLine = (
+        <ResponsiveLine
+            data={data}
+            margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+            xScale={{ type: 'point' }}
+            yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+                orient: 'bottom',
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: 'transportation',
+                legendOffset: 36,
+                legendPosition: 'middle'
+            }}
+            axisLeft={{
+                orient: 'left',
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: 'count',
+                legendOffset: -40,
+                legendPosition: 'middle'
+            }}
+            colors={{ scheme: 'nivo' }}
+            pointSize={10}
+            pointColor={{ theme: 'background' }}
+            pointBorderWidth={2}
+            pointBorderColor={{ from: 'serieColor' }}
+            pointLabel="y"
+            pointLabelYOffset={-12}
+            useMesh={true}
+            legends={[
+                {
+                    anchor: 'bottom-right',
+                    direction: 'column',
+                    justify: false,
+                    translateX: 100,
+                    translateY: 0,
+                    itemsSpacing: 0,
+                    itemDirection: 'left-to-right',
+                    itemWidth: 80,
+                    itemHeight: 20,
+                    itemOpacity: 0.75,
+                    symbolSize: 12,
+                    symbolShape: 'circle',
+                    symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                    effects: [
+                        {
+                            on: 'hover',
+                            style: {
+                                itemBackground: 'rgba(0, 0, 0, .03)',
+                                itemOpacity: 1
+                            }
+                        }
+                    ]
+                }
+            ]}
+        />
+    )
+    return (
+      <div style={{height:500,width:600}}>
+        {MyResponsiveLine}
+      </div>
+    )
   }
 
   async componentDidMount(){
@@ -75,12 +416,15 @@ class Home extends Component{
     }
     const historicalDataRef = await this.props.firebase.getHistoricalData()
     if (historicalDataRef.exists){
+      console.log("Getting historicalData")
       const historicalData = historicalDataRef.data().all
       this.setState({historicalData:historicalData})
+
     }
 }
 
   render (){
+    console.log(this.state.historicalData)
     return (
       <div style={{display: 'flex',flexDirection: 'column',alignItems: 'center'}}>
         <div style={{display:'flex',flexDirection:'row',paddingTop: 2}}>
@@ -117,6 +461,7 @@ class Home extends Component{
           {getDataBlock("data",this.state.testsInProgress,0,0,15)}
           {getDataBlock("data",this.state.conductedTests,0,0,0,15)}
         </div>
+        {this.getLineChart()}
 
       </div>
     );
